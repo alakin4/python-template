06f94f5de860d960a82a95a41ff91ef277dfddcc
Alan Kinene <alakin4@gmail.com>
2025-04-21 17:05:23 -0500

    add main entry point script

---
+++ main.py
@@ -0,0 +1,13 @@
+def main() -> None:
+    """
+    Main function to execute the script.
+
+    This function serves as the entry point for the script and contains
+    the main logic. It can be modified to include any functionality
+    required for the project.
+    """
+    print("Hello, World! this is a template for a Python projects.")
+
+
+if __name__ == "__main__":
+    main()
